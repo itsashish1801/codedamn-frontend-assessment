85e37fe89c97ab90fed29f2dc112d5d703b43bd0
Ashish Sharma <itsashishsharma1801@gmail.com>
2023-04-07 14:51:06 +0530

    Fetch data in Index page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,17 @@
+import useSWR from 'swr';
+
 export default function Home() {
+  const { data, error } = useSWR('/api/user', (url: string) =>
+    fetch(url).then((res) => res.json())
+  );
+
+  if (error) return <div>Failed to load</div>;
+  if (!data) return <div>Loading...</div>;
+
   return (
     <main>
-      <h1>Codedamn frontend assessment</h1>
+      <h1>Hi</h1>
+      <p>{data.fullName}</p>
     </main>
   );
 }
