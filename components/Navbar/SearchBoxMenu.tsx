1e6e51bf4cd6bf92580edc1bcf0cad9a189b7be1
Ashish Sharma <itsashishsharma1801@gmail.com>
2023-04-24 10:25:31 +0530

    Change SearchBox posn via translate.

--- components/Navbar/SearchBoxMenu.tsx
+++ components/Navbar/SearchBoxMenu.tsx
@@ -19,7 +19,10 @@ function SearchBoxMenu() {
 
   return (
     <div>
-      <Menu as='div' className='relative inline-block mr-2 text-left md:mr-0'>
+      <Menu
+        as='div'
+        className='relative inline-block text-left -translate-x-2 md:translate-x-0'
+      >
         <div>
           <Menu.Button className='inline-flex items-center w-full px-2 py-1 text-sm rounded-lg group bg-zinc-100 text-zinc-600 hover:bg-zinc-200 hover:text-zinc-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-zinc-500'>
             {selectedItem}
